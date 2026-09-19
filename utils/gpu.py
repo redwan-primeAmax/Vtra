1e9abbc515f6d1e18a13ffc6925a43e4a7722348
redwan-primeAmax <redwanislam7900@gmail.com>
2026-09-19 11:25:15 +0600

    update

--- utils/gpu.py
+++
@@ -1,9 +0,0 @@
-import gc
-import torch
-
-def clear_vram():
-    """VRAM খালি করার মেমোরি ক্লিয়ারেন্স ফাংশন"""
-    gc.collect()
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-        torch.cuda.ipc_collect()
